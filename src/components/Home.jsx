15e85734fa8ded3ca4b9fb11c24cac4f963d150a
Isha Mahesh Mahajan <ishamaheshmahajan@Ishas-MacBook-Air.local>
2024-11-17 19:48:45 +0530

    init3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <a href="/correct">Go to Correct</a>
+      <br />
+      <a href="/incorrect">Go to Incorrect</a>
+    </div>
   )
 }
 
